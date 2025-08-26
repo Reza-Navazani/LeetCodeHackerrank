a11b4b330f02237ee4d3efba126f46864408fe44
Reza <mre.navazani@gmail.com>
2025-08-25 17:48:40 -0700

    Palindrome Numbers

---
+++ Solutions/Palindrome_Number.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if (x < 0) return false; // negative numbers are not palindromes
+        
+        int original = x;
+        int reversed = 0;
+        
+        while (x > 0) {
+            int r = x % 10;
+            x = x / 10;
+            reversed = reversed * 10 + r;
+        }
+        
+        return reversed == original;
+    }
+    public void Main1()
+    {
+        IsPalindrome(121);
+        IsPalindrome(-121);
+
+    }
+}
+
+
+
